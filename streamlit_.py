48777250d7ee3f600b8c6eaed3aaed2653cfbf85
lokesh-kumar-m <mekalalokesh1999@gmail.com>
2023-10-03 20:04:28 +0530

    adding s3 file

--- streamlit_.py
+++ streamlit_.py
@@ -1,3 +1,4 @@
+import pandas
 import streamlit
 streamlit.title("Fruit Smoothies")
 streamlit.header("Smoothie Menu")
@@ -6,3 +7,5 @@ streamlit.text("Grapes")
 streamlit.text("Banana milkshake")
 streamlit.text("Strawberry Chocolate")
 streamlit.text("Double chocolate")
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
